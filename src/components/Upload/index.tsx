30ca04dc9db356bf72a9ab21205bface8d2f86a4
João Gabriel Pancheski <joao.ti@grupointegrado.br>
2020-04-23 00:42:26 -0300

    Implements import page connection with api

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -4,14 +4,14 @@ import Dropzone from 'react-dropzone';
 import { DropContainer, UploadMessage } from './styles';
 
 interface UploadProps {
-  onUpload: Function;
+  onUpload: any;
 }
 
 const Upload: React.FC<UploadProps> = ({ onUpload }: UploadProps) => {
   function renderDragMessage(
     isDragActive: boolean,
     isDragRejest: boolean,
-  ): ReactNode {
+  ): any {
     if (!isDragActive) {
       return (
         <UploadMessage>Selecione ou arraste o arquivo aqui.</UploadMessage>
@@ -27,7 +27,7 @@ const Upload: React.FC<UploadProps> = ({ onUpload }: UploadProps) => {
 
   return (
     <>
-      <Dropzone accept="text/csv" onDropAccepted={(files) => onUpload(files)}>
+      <Dropzone accept="application/vnd.ms-excel" onDropAccepted={onUpload}>
         {({ getRootProps, getInputProps, isDragActive, isDragReject }): any => (
           <DropContainer
             {...getRootProps()}
